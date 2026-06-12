12376b2eddbfbf6effb9e40ea2dbdb88177b3b2c
Illia Redka <illaredka8@gmail.com>
2026-06-12 21:52:12 +0300

    Add mange user endpoint

--- users/urls.py
+++ users/urls.py
@@ -4,12 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from users.views import CreateUserView
+from users.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("", CreateUserView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "users"
