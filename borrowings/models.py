efe6599b1b929697efd74cac339c6a2cc0c156d5
Illia Redka <illaredka8@gmail.com>
2026-06-13 19:08:10 +0300

    Add constraints for borrow_date, expected_return_date and actual_return_date

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import F, Q
 from django.conf import settings
 
 from books.models import Book
@@ -15,6 +16,19 @@ class Borrowing(models.Model):
 
     class Meta:
         ordering = ("-borrow_date",)
+        constraints = [
+            models.CheckConstraint(
+                condition=Q(expected_return_date__gt=F("borrow_date")),
+                name="expected_return_date_after_borrow_date",
+            ),
+            models.CheckConstraint(
+                condition=(
+                    Q(actual_return_date__isnull=True)
+                    | Q(actual_return_date__gte=F("borrow_date"))
+                ),
+                name="actual_return_date_after_or_equal_borrow_date",
+            ),
+        ]
 
     def __str__(self):
         return f"{self.user} borrowed {self.book}"
--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from borrowings.models import Borrowing
 from books.serializers import BookSerializer
@@ -17,6 +18,35 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+
+        borrow_date = attrs.get(
+            "borrow_date", getattr(self.instance, "borrow_date", None)
+        )
+        expected_return_date = attrs.get(
+            "expected_return_date", getattr(self.instance, "expected_return_date", None)
+        )
+        actual_return_date = attrs.get(
+            "actual_return_date", getattr(self.instance, "actual_return_date", None)
+        )
+
+        if borrow_date and expected_return_date and expected_return_date <= borrow_date:
+            raise ValidationError(
+                {
+                    "expected_return_date": "Expected return date must be after borrow date."
+                }
+            )
+
+        if borrow_date and actual_return_date and actual_return_date < borrow_date:
+            raise ValidationError(
+                {
+                    "actual_return_date": "Actual return date cannot be before borrow date."
+                }
+            )
+
+        return attrs
 
 
 class BorrowingListSerializer(BorrowingSerializer):
