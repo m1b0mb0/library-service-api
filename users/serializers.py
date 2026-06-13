c224e1e9491d62235e7dc17b222b9a84b5e9a550
Illia Redka <illaredka8@gmail.com>
2026-06-13 16:31:10 +0300

    Add users service tests

--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "password", "is_staff")
+        fields = ("id", "email", "first_name", "last_name", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
             "password": {
